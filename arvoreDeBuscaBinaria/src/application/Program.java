4fe1c3c62a56807551db5ce54dead12c2dca2050
Davison Lucas <dlucasmv@gmail.com>
2022-05-15 01:58:40 -0300

    Binary Tree, Data Structures. First Commit.

---
+++ arvoreDeBuscaBinaria/src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import elements.ArvoreBinaria;
+import model.Obj;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ArvoreBinaria<Obj> arvore = new ArvoreBinaria<>();
+
+	    arvore.insert(new Obj(13));
+	    arvore.insert(new Obj(10));
+	    arvore.insert(new Obj(25));
+	    arvore.insert(new Obj(2));
+	    arvore.insert(new Obj(12));
+	    arvore.insert(new Obj(20));
+	    arvore.insert(new Obj(31));
+	    arvore.insert(new Obj(29));
+
+	    arvore.exibirPreOrdem();
+	    System.out.println();
+	    
+	    arvore.exibirInOrdem();
+	    System.out.println();
+	    
+	    arvore.exibirPosOrdem();
+	    System.out.println();
+	    
+	    arvore.exibirFolhas();
+	    System.out.println();
+	    
+	    System.out.println(arvore.high());
+	    
+	    System.out.println(arvore.contarNos());
+	    
+	    System.out.println(arvore.extratamenteBinaria());
+		
+
+	}
+
+}
